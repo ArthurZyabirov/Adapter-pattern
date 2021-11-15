c2c752a293ae4267805041a0a512bd76a14f9bb1
Arthur Zyabirov <zyabirow@gmail.com>
2021-11-16 00:35:16 +0300

    calc changes

--- src/main/java/IntsCalculator.java
+++ src/main/java/IntsCalculator.java
@@ -5,16 +5,18 @@ public class IntsCalculator implements Ints {
 
         @Override
         public int sum(int arg0, int arg1) {
-            return arg0 + arg1;
+            return (int) target.newFormula().addOperand(arg0).addOperand(arg1).calculate(Calculator.Operation.SUM).result();
+
+
         }
 
         @Override
         public int mult(int arg0, int arg1) {
-            return  arg0 * arg1;
+            return (int) target.newFormula().addOperand(arg0).addOperand(arg1).calculate(Calculator.Operation.MULT).result();
         }
 
         @Override
         public int pow(int a, int b) {
-            return (int) Math.pow(a,b);
+            return (int) target.newFormula().addOperand(a).addOperand(b).calculate(Calculator.Operation.POW).result();
         }
 }
